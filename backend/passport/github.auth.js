070d57a3d7b51dfa4bd04104f5a7406c204d7d0a
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-05-10 18:48:23 +0300

    Callback url updated

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/github/callback",
+      callbackURL:
+        "https://github-app-f6sd.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       const user = await User.findOne({ username: profile.username });
